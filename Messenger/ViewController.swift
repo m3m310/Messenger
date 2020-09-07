7f518b55278e2a60dc071fd934333e69d25cef47
mimi <s101124020@gmail.com>
2020-09-07 18:40:42 +0300

    initial getting started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Meme Mhd on 07/09/2020.
+//  Copyright © 2020 meme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
